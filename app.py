93b9dec7673ca7f4e9a83728579156ed4d533f23
Akash Pradhan <akashpradhan91999@gmail.com>
2026-07-26 15:11:19 +0530

    Add debugging messages to app

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 st.sidebar.title("Whatsapp Chat Analyzer")
 
 
-
+uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     data = bytes_data.decode("utf-8")
